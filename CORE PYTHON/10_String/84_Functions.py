1ec18f03e30b9b6721851c2aa96dd76a7a00850b
harshy261 <iyadavharsh261@gmail.com>
2022-12-09 23:36:54 +0530

    Ninth Commit

---
+++ CORE PYTHON/10_String/84_Functions.py
@@ -0,0 +1,136 @@
+# STRING FUNCTIONS
+
+
+# # upper()   -- to UpperCase
+# name = "harsh"
+# print(name)
+# print(name.upper())
+# str1 = name.upper()
+# print(str1)
+
+
+# # lower()   -- to LowerCase
+# name = "harsh"
+# print(name)
+# print(name.lower())
+
+
+# # swapcase()    -- Upper to Lower and Lower to Upper
+# name = "harsh"
+# print(name)
+# str1 = name.swapcase()
+# print(str1)
+
+
+# # title()   -- make first letter capital
+# name = "hello harsh how are you"
+# str1 = name.title()
+# print(name)
+# print(str1)
+
+
+# # isupper()   -- upperCase or not
+# name = "HARSHYADAV"
+# str1 = name.isupper()
+# print(str1)
+
+
+# # islower()   -- lowerCase or not
+# name = "HARSHYADAV"
+# str2 = name.islower()
+# print(str2)
+
+
+# # istitle()   -- title or not
+# name = "HARSHYADAV"
+# str3 = name.istitle()
+# print(str3)
+
+
+# # isdigit()   -- only number or not
+# name1 = "372366bbj4423"
+# name2 = "3723664423"
+# print(name1.isdigit())
+# print(name2.isdigit())
+
+
+# # isalpha()   -- only letters or not
+# name1 = "HarshYadav2321"
+# name2 = "HarshYadav2321"
+# print(name1.isalpha())
+# print(name2.isalpha())
+
+
+# # isalnum()   -- checks if all is num or not
+# name1 = "HarshYadav2321"
+# name2 = "2321"
+# print(name1.isalnum())
+# print(name2.isalnum())
+
+
+# # isspace()   -- space or not 
+# name1 = "       shashd"
+# print(name1.isspace())
+
+
+# # lstrip()   -- remove space from left side of string 
+# name = "    HarshYadav"
+# str1 = name.lstrip()
+# print(name)
+# print(str1)
+
+
+# # rstrip()   -- remove space from right side of string 
+# name = "HarshYadav      "
+# str1 = name.rstrip()
+# print(name)
+# print(str1)
+
+
+# # strip()   -- remove space from both side of string 
+# name = "    HarshYadav      "
+# str1 = name.strip()
+# print(name)
+# print(str1)
+
+
+# # replace()     -- replace a sub string in a string with another sub string
+# name = "HarshYadav"
+# old = "Sangam"
+# new = "Kumar"
+# name.replace(old,new)
+# name.replace("Sangam","Kumar")
+# print(name)
+
+
+# # split()     -- removes certain element from string
+# name = "Harsh-Sangam-Yadav-dawdw"
+# str1 = name.split('-')
+# print(name)
+# print(str1)
+
+# name2 = "Harsh Yadav"
+# str2 = name2.split(' ')
+# print(name2)
+# print(str2)
+
+
+# # join()     -- adds certain element from string
+name = ('Hello', 'my', 'name', 'is', 'Harsh')
+str1 = '_'.join(name)
+print(name)
+print(str1)
+
+name2 = ('Hello', 'my', 'name', 'is', 'Harsh')
+str2 = ' '.join(name2)
+print(name2)
+print(str2)
+
+
+
+
+
+
+
+
+
