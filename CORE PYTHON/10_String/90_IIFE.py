a5474e9a81d19f4cf53b94ad0a191d0da15a8891
harshy261 <iyadavharsh261@gmail.com>
2022-12-13 23:10:43 +0530

    Eleventh Commit

---
+++ CORE PYTHON/10_String/90_IIFE.py
@@ -0,0 +1,7 @@
+# IMMEDIATELY INVOKED FUNCTION EXPRESSIONS
+
+sum = lambda x:x+1
+sum(5)
+
+(lambda x : print(x+1)) (5)
+(lambda x, y : print(x+y)) (5, 6)
---
+++ CORE PYTHON/10_String/91_Variables.py
@@ -0,0 +1,11 @@
+# GLOBAL AND LOCAL VARIABLES
+
+a = 50              # Global Variable
+def show():
+    x = 10          # Local Variable
+    print(a)
+    print(x)
+
+show()
+print("x:",x)
+print("a:",a)
