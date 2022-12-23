94ea5111be4a98ebd7b32c921b7f069c1a98c2d3
harshy261 <iyadavharsh261@gmail.com>
2022-12-23 19:24:39 +0530

    27th Commit

---
+++ CORE PYTHON/13_Tuple/122_Tuple.py
@@ -0,0 +1,59 @@
+# TUPLE
+
+# contains a group of element which can be same or different types.
+# immutable
+# similar to list but Tuples are read-only which means we can not modify it's element.
+# used to store data which should not be modified.
+# occupies less memory compare to list.
+# represented using parentheses ()
+
+a = (10)
+print(a)
+print(type(a))
+
+b = (10, 20, 30, 40)
+print(b)
+print(type(b))
+
+c = (10, 20, -30, 40.34, 'HarshYadav')
+print(c)
+print(type(c))
+
+print(c[0])
+print(c[1])
+print(c[2])
+print(c[3])
+print(c[4])
+
+print()
+print()
+
+print(c[-1])
+print(c[-2])
+print(c[-3])
+print(c[-4])
+print(c[-5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
