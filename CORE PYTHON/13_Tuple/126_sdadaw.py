ccbd4157bf3103efc396f385072870f008228eb4
harshy261 <iyadavharsh261@gmail.com>
2022-12-27 20:26:37 +0530

    30th Commit

--- CORE PYTHON/13_Tuple/126_sdadaw.py
+++ CORE PYTHON/13_Tuple/126_sdadaw.py
@@ -8,19 +8,6 @@ for i in range(n):
     print(i, "=", x[i])
 print()
 
-print("From 1st Position to 4th Position")
-a = x[1:5]
-for i in a:
-    print(i)
-print()
-
-print("From 0th Position to last Position")
-b = x[0:]
-for i in b:
-    print(i)
-print()
-
-
 
 
 
