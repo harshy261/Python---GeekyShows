8700ac30f6cacd3c028fbed677aa8a288a5517bc
harshy261 <iyadavharsh261@gmail.com>
2023-02-04 22:55:16 +0530

    Threading commit

--- ADVANCED PYTHON/8_Threading/33_Multitasking.py
+++ ADVANCED PYTHON/8_Threading/33_Multitasking.py
@@ -9,6 +9,64 @@
 # When we start a Python Program , one thread begins running immediately, which is called Main thread of that program created by PVM.
 # Created automatically when program is started.
 
+from threading import Thread
 
+
+# #1
+# def disp():
+#     print("Thread Running")
+
+# t = Thread(target = disp)
+# t.start()
+
+
+
+# #2
+# def disp(a, b):
+#     print("Thread Running ", a, b)
+
+# for i in range(5):
+#     t = Thread(target = disp, args=(10, 20))
+    
+#     t.start()
+
+
+
+
+
+# #3
+# def disp(a, b):
+#     print("Thread Running ", a, b)
+
+# t = Thread(target = disp, args=(10, 20))
+    
+# t.start()
+
+
+
+# #4
+# def disp():
+#     for i in range(5):
+#         print("Child Thread")
+
+# t = Thread(target = disp)
+
+# t.start()
+
+# for i in range(5):
+#     print("Main Thread")
+
+
+# #5
+def disp():
+    for i in range(5):
+        print("Publish video C")
+
+t = Thread(target = disp)
+
+t.start()
+
+for i in range(5):
+    print("Publish Video M")
 
 
