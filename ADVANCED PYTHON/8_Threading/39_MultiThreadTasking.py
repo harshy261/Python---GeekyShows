dd51ade222ec3025372ade0649dc4701721489c7
harshy261 <iyadavharsh261@gmail.com>
2023-06-27 19:54:06 +0530

    MultiThread

--- ADVANCED PYTHON/8_Threading/39_MultiThreadTasking.py
+++ ADVANCED PYTHON/8_Threading/39_MultiThreadTasking.py
@@ -24,27 +24,3 @@ t2.start()
 
 
 
-
-from threading import *
-
-class Hotel:
-    def __init__(self, t):
-        self.t = t
-    
-    def food(self):
-        for i in range(1, 6):
-            print(self.t, i)
-
-h1 = Hotel('Take order from Table ')
-h2 = Hotel('Serve order to Table ')
-
-t1 = Thread(target=h1.food)
-t2 = Thread(target=h2.food)
-
-t1.start()
-t2.start()
-
-
-
-
-
